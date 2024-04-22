551745e6a297323af8e48c824fe126bfb0505744
akshat-315 <akshat315.sh@gmail.com>
2024-04-22 11:04:53 +0530

    Created Sign-up page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import SignUp from "./pages/SignUp";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/sign-up" element={<SignUp />} />
       </Routes>
       <Footer />
     </BrowserRouter>
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,8 +6,8 @@ import linkedin from "../assets/linkedIn.png";
 
 const Footer = () => {
   return (
-    <div className="bg-[#c8c8c8] px-6 py-16">
-      <div className="flex sm:flex-col justify-center items-center gap-12">
+    <div className="bg-[#c8c8c8] px-6 py-16 max-h-fit">
+      <div className="flex flex-col justify-center items-center gap-12">
         <div className="flex gap-2 w-1/4 px-40 justify-center">
           <img src={icon} width={28} height={30} />
           <h2 className="text-4xl text-gray-800">todo</h2>
@@ -16,17 +16,17 @@ const Footer = () => {
           <div className="flex flex-col gap-2">
             <h2>Features</h2>
             <Link className="no-underline ">
-              <button className=" bg-[#c8c8c8] hover:bg-[#9e9e9e] text-gray-600 border-none">
+              <button className=" bg-[#c8c8c8] hover:bg-[#9e9e9e] text-gray-600 border-none cursor-pointer">
                 How it works <span> </span>
               </button>
             </Link>
             <Link className="no-underline ">
-              <button className=" bg-[#c8c8c8] hover:bg-[#9e9e9e] text-gray-600 border-none">
+              <button className=" bg-[#c8c8c8] hover:bg-[#9e9e9e] text-gray-600 border-none cursor-pointer">
                 Pricing
               </button>
             </Link>
             <Link className="no-underline ">
-              <button className=" bg-[#c8c8c8] hover:bg-[#9e9e9e] text-gray-600 border-none">
+              <button className=" bg-[#c8c8c8] hover:bg-[#9e9e9e] text-gray-600 border-none  cursor-pointer">
                 Templates
               </button>
             </Link>
@@ -34,17 +34,17 @@ const Footer = () => {
           <div className="flex flex-col gap-2">
             <h2>Resources</h2>
             <Link className="no-underline ">
-              <button className=" bg-[#c8c8c8] hover:bg-[#9e9e9e] text-gray-600 border-none">
+              <button className=" bg-[#c8c8c8] hover:bg-[#9e9e9e] text-gray-600 border-none cursor-pointer">
                 How it works <span> </span>
               </button>
             </Link>
             <Link className="no-underline ">
-              <button className=" bg-[#c8c8c8] hover:bg-[#9e9e9e] text-gray-600 border-none">
+              <button className=" bg-[#c8c8c8] hover:bg-[#9e9e9e] text-gray-600 border-none cursor-pointer">
                 Pricing
               </button>
             </Link>
             <Link className="no-underline ">
-              <button className=" bg-[#c8c8c8] hover:bg-[#9e9e9e] text-gray-600 border-none">
+              <button className=" bg-[#c8c8c8] hover:bg-[#9e9e9e] text-gray-600 border-none cursor-pointer">
                 Templates
               </button>
             </Link>
@@ -52,31 +52,31 @@ const Footer = () => {
           <div className="flex flex-col gap-2">
             <h2>Company</h2>
             <Link className="no-underline ">
-              <button className=" bg-[#c8c8c8] hover:bg-[#9e9e9e] text-gray-600 border-none">
+              <button className=" bg-[#c8c8c8] hover:bg-[#9e9e9e] text-gray-600 border-none cursor-pointer">
                 How it works <span> </span>
               </button>
             </Link>
             <Link className="no-underline ">
-              <button className=" bg-[#c8c8c8] hover:bg-[#9e9e9e] text-gray-600 border-none">
+              <button className=" bg-[#c8c8c8] hover:bg-[#9e9e9e] text-gray-600 border-none cursor-pointer">
                 Pricing
               </button>
             </Link>
             <Link className="no-underline ">
-              <button className=" bg-[#c8c8c8] hover:bg-[#9e9e9e] text-gray-600 border-none">
+              <button className=" bg-[#c8c8c8] hover:bg-[#9e9e9e] text-gray-600 border-none ">
                 Templates
               </button>
             </Link>
           </div>
-          <div className="sm:flex  gap-12 md:flex-col  md:gap-6">
-            <div>
+          <div className="sm:flex sm:gap-6 md:flex-col  md:gap-6">
+            <button className="bg-transparent border-none cursor-pointer hover:bg-[#b4b4b4] ">
               <img src={insta} alt="Instagram GIF" width={20} />
-            </div>
-            <div>
+            </button>
+            <button className="bg-transparent border-none cursor-pointer hover:bg-[#b4b4b4]">
               <img src={twitter} alt="Instagram GIF" width={20} />
-            </div>
-            <div>
+            </button>
+            <button className="bg-transparent border-none cursor-pointer hover:bg-[#b4b4b4]">
               <img src={linkedin} alt="Instagram GIF" width={20} />
-            </div>
+            </button>
           </div>
           <div></div>
         </div>
